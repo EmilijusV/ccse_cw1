67d3f0ebd22f6a466395dc1b6bd897b52c54f7c6
EmilijusV <Emilijus.vaskevicius@warwick.ac.uk>
2024-01-12 12:34:58 +0000

    Added some additions

--- Services/ApplicationDbContext.cs
+++ Services/ApplicationDbContext.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace ccse_cw1.Services
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext: IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
