7f72f8fe1c1ebea6977f214b14240a585ee395b0
EmilijusV <Emilijus.vaskevicius@warwick.ac.uk>
2024-01-12 19:26:20 +0000

    Touching up Register Page

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -71,6 +71,14 @@ namespace ccse_cw1.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            [Required]
+            public string FirstName { get; set; }
+            [Required]
+            public string LastName { get; set; }
+            [Required]
+            public string PhoneNumber { get; set; }
+            [Required]
+            public string Address { get; set; }
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
