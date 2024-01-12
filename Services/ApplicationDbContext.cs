c7d0ff7fb40baeb58af2eed3a65f45413a3320ef
EmilijusV <Emilijus.vaskevicius@warwick.ac.uk>
2024-01-12 14:10:53 +0000

    Deleted migrations and added ApplicationUser

--- Services/ApplicationDbContext.cs
+++ Services/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using ccse_cw1.Models;
 
 namespace ccse_cw1.Services
 {
-    public class ApplicationDbContext: IdentityDbContext
+    public class ApplicationDbContext: IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
