d5f2dae8ac202e289bbcea52cbb054342e2aac7b
EmilijusV <Emilijus.vaskevicius@warwick.ac.uk>
2024-01-21 17:58:30 +0000

    Hotel Booking System working

--- Pages/Booking.cshtml.cs
+++ Pages/Booking.cshtml.cs
@@ -7,24 +7,24 @@ using Microsoft.AspNetCore.Identity;
 using ccse_cw1.Services;
 using Microsoft.AspNetCore.Authentication;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ccse_cw1.Pages
 {
+    [Authorize]
     [BindProperties]
     public class BookingModel : PageModel
     {
         private readonly BookingSystem _context;
 
-       // private readonly UserManager<ApplicationUser> _userManager; // user id
+        private readonly UserManager<ApplicationUser> _userManager; // user id
 
-        public BookingModel(BookingSystem context)
+        public BookingModel(BookingSystem context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
-        }
-        /*public BookingModel(UserManager<ApplicationUser> userManager)
-        {
             _userManager = userManager;
-        }*/
+        }
+
         public IList<Hotel> BookingSystems { get; set; }
         public IList<HotelDate> HotelDates { get; set; }
 
@@ -32,25 +32,24 @@ namespace ccse_cw1.Pages
 
         public double TotalPrice { get; set; }
 
-        /*        public async Task<string> GetUserIdAsync()
-                {
-                    ApplicationUser user = await _userManager.GetUserAsync(User);
-
-                    if (user != null)
-                    {
-                        return user.Id;
-                    }
-                    return null;
-                }*/
         public async Task OnGetAsync()
         {
             BookingSystems = await _context.Hotels.ToListAsync();
             HotelDates = await _context.HotelDates.ToListAsync();
         }
-
+        
         public async Task<IActionResult> OnPost(HotelBooking hotelBooking)
         {
+            var user= await _userManager.GetUserAsync(User);
 
+            if (user != null) 
+            {
+                hotelBooking.CustomerID = user.Id;
+            }
+            else
+            {
+                return RedirectToPage("Error");
+            }
             await _context.HotelBookings.AddAsync(hotelBooking);
             await _context.SaveChangesAsync();
             return RedirectToPage("Index");
